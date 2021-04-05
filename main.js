c255801961571373acd1823fabe12223923ec63e
VladimirMalkov <vladimir.malkov@tappysoft.com>
2021-04-05 11:53:12 +0300

    fix

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-// // We listen to the resize event
-// window.addEventListener('resize', () => {
-//   // We execute the same script as before
-//   let vh = window.innerHeight * 0.01;
-//   document.documentElement.style.setProperty('--vh', `${vh}px`);
-// });
-
 // countdown
 (function () {
   const second = 1000,
@@ -39,3 +32,9 @@
       //seconds
     }, 0)
 }());
+
+// Fix for iphone
+if (navigator.platform === 'iPhone') {
+  const navigation = document.getElementById('navigation');
+  navigation.style.marginBottom = '10vh';
+}
